47cd789ecb768483630164d7b269de7e269ca46b
TilmanK <tilman.krummeck@googlemail.com>
2019-03-28 10:24:12 +0100

    Created setup script for tsl library.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tsl',
+      version='1.0',
+      description='Library for TSL shared functions',
+      url='https://tfs.itgr.net/TPS/TSL/_dashboards',
+      author='Walid Amokrane, Tilman Krummeck',
+      author_email='walid.amokrane@tuev-sued.de',
+      license='MIT',
+      packages=['tsl'],
+      zip_safe=False)
