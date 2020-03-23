11d1cb7e15ccf04a0493fcb590a7951366fa046f
Krummeck, Tilman <Tilman.Krummeck@tuev-sued.de>
2020-03-23 16:44:42 +0100

    PR 18 (Fixed logging in helper functions TSL lib and removed create_path_map method #3948 #3899) gemergt
    - Removed obsolete create_path_map function from helper functions #3899
    - Fixed logging in helper functions to use tsl logger
    - Added type annotations for helper functions
    - Refactored helper functions to not use f-strings in logging
    
    Verwandte Arbeitselemente: 3899, 3948

--- tsl/helper.py
+++ tsl/helper.py
@@ -9,20 +9,22 @@ Public methods:
 - get_process_path: Search for the process folder based on the given id.
 - create_path_map: Create a map of ians and paths.
 """
-import re
 from datetime import date
-from logging import info
+import logging
 import os
+from typing import Dict
 
 from PyQt5.QtCore import QByteArray, QBuffer, QIODevice
 from PyQt5.QtGui import QPixmap
 
 PATH = r"\\de001.itgr.net\PS\RF-UnitCentralPS_PSE\CPS"
 
-MEMO = {"process": {}, "project": {}}
+MEMO: Dict[str, Dict[int, str]] = {"process": {}, "project": {}}
+
+log = logging.getLogger("tsl")  # pylint: disable=invalid-name
 
 
-def encode_pixmap(pixmap):
+def encode_pixmap(pixmap: QPixmap) -> str:
     """
     Encode a QPixmap to a Base64 str.
 
@@ -36,7 +38,7 @@ def encode_pixmap(pixmap):
     return byte_array.toBase64().data().decode("utf-8")
 
 
-def decode_pixmap(pixmap_string):
+def decode_pixmap(pixmap_string: str) -> QPixmap:
     """
     Decode a QPixmap from a Base64 str.
 
@@ -49,7 +51,7 @@ def decode_pixmap(pixmap_string):
     return qpixmap
 
 
-def get_project_path(project_id):
+def get_project_path(project_id: int) -> str:
     """
     Search for the project folder based on an id.
 
@@ -63,7 +65,7 @@ def get_project_path(project_id):
     return get_path_for_id(project_id, "project")
 
 
-def get_process_path(process_id):
+def get_process_path(process_id: int) -> str:
     """
     Search for the process folder based on an id.
 
@@ -77,7 +79,7 @@ def get_process_path(process_id):
     return get_path_for_id(process_id, "process")
 
 
-def get_path_for_id(ident, id_type):
+def get_path_for_id(ident: int, id_type: str) -> str:
     """
     Get the project path for the given id and type.
 
@@ -86,11 +88,11 @@ def get_path_for_id(ident, id_type):
     :raises ValueError: if no path was found for given id
     :return: <class tr> path for id
     """
-    info(f"Searching correct path for {id_type} id %s", ident)
+    log.info("Searching correct path for %s id %s", id_type, ident)
     year = date.today().year
 
     try:
-        info("Trying to returned cached entry.")
+        log.info("Trying to returned cached entry.")
         return MEMO[id_type][ident]
     except KeyError:
         for i in range(0, year - 2000):
@@ -100,41 +102,8 @@ def get_path_for_id(ident, id_type):
             if os.path.exists(path):
                 id_path = os.path.join(path, str(ident))
                 if os.path.exists(id_path):
-                    info(f"Found {id_type} path {id_path}")
+                    log.info("Found %s path %s", id_type, id_path)
                     MEMO[id_type][ident] = id_path
                     return id_path
 
     raise ValueError(f"No path found for {id_type} id {ident}")
-
-
-def create_path_map():
-    """
-    Create a map of ians and paths.
-
-    :return: <class dict> ians with their respective paths
-    """
-    # there is not better way doing this, an extra function also makes no sense
-    # pylint: disable=too-many-nested-blocks
-    paths = {}
-    for i in range(2):
-        path = os.path.join(os.path.join(PATH, "PSEX\\Prozesse"),
-                            str(date.today().year - i))
-
-        # create a list of all folders sorted by their INODE number
-        folders = [(folder, os.stat(os.path.join(path, folder)).st_ino)
-                   for folder in os.listdir(path)]
-        folders.sort(key=lambda x: x[1])
-
-        for fold in folders:
-            try:
-                for file in os.listdir(os.path.join(path, fold[0], "SPEC")):
-                    if re.search("_PA_Stand", file) or \
-                            re.search("_Anlage EAN-Codes", file):
-                        try:
-                            paths[int(file[:6])] = fold[0]
-                            break
-                        except ValueError:
-                            continue
-            except FileNotFoundError:
-                continue
-    return paths
--- tsl/version.py
+++ tsl/version.py
@@ -1,2 +1,2 @@
 """Version definition for TSL Library."""
-VERSION = "1.0.4"
+VERSION = "1.0.5"
