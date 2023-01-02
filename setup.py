0872bc82f5d7c4b08d10f63f3e5da94958ae5d36
Patrick Zwerschke <patrick@p5k.org>
2023-01-02 13:37:37 +0100

    added setup.py back in for none poetry installs

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""Setup script for QuteStyle library."""
+import importlib.metadata
+
+from setuptools import setup
+
+VERSION = importlib.metadata.version("qute_style")
+
+setup(
+    name="qute_style",
+    version=VERSION,
+    description="An expandable application framework for Qt",
+    url="https://github.com/TUV-SUD-Product-Service-GmbH/QuteStyle",
+    author="Marina Baumgartner, Dairen Gonschior, Tilman Krummeck, "
+    "Alexander Schwarz, Dennis Spitzhorn, Gerhard Trapp, Patrick Zwerschke",
+    author_email="PS-TF-Entwicklung@tuev-sued.de",
+    license="MIT",
+    packages=[
+        "qute_style",
+        "qute_style.gen",
+        "qute_style.dev",
+        "qute_style.widgets",
+    ],
+    package_data={
+        "qute_style": ["py.typed", "widgets/**/*.py"],
+    },
+    install_requires=["PyQt5", "pyqtspinner"],
+    zip_safe=False,
+)
