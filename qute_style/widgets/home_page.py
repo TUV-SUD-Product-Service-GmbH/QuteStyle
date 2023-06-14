baffcdde64b57c4554f41af201ab56569c441e5f
getr11 <gerhard.trapp@googlemail.com>
2023-06-14 14:36:32 +0200

    Added date to changelog

--- qute_style/widgets/home_page.py
+++ qute_style/widgets/home_page.py
@@ -282,12 +282,12 @@ class HomePage(MainWidget):
 
         row = 0
         for version_info, log_data_dict in change_log_data.items():
-            version_info = (
+            version_info_text = (
                 f"V {version_info.version} - {version_info.release_date}"
                 if version_info.release_date
                 else f"V {version_info.version}"
             )
-            label = QLabel(version_info)
+            label = QLabel(version_info_text)
             label.setObjectName("heading1_label")
             label.setMinimumHeight(30)
             grid_layout.addWidget(label, row, 1, 1, 2)
